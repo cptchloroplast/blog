e44a11a9656eb879b40388dd96ea6e02667654d8
Benjamin Okkema <ben@elm.okkema.org>
2020-06-06 16:19:14 -0500

    fix env loading + fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ ENV PATH="/opt/venv/bin:$PATH"
 RUN pip install -r requirements/prod.txt
 
 FROM python:3.7-alpine as release
+RUN apk add libpq
 COPY --from=build /opt/venv /opt/venv
 ENV PATH="/opt/venv/bin:$PATH"
-COPY app/* app/
+WORKDIR /home
+COPY app ./app
 COPY .env ./
-COPY data.json ./
 CMD exec gunicorn -b :$PORT app:app
--- app/config.py
+++ app/config.py
@@ -4,13 +4,12 @@ from dotenv import find_dotenv, load_dotenv
 
 class Config():
 
-    def __init__(self):
-        load_dotenv(find_dotenv())
+    load_dotenv(find_dotenv())
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
     RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
 
-    DATA_PATH = os.path.abspath(os.path.join(__file__, '../../data.json'))
+    DATA_PATH = os.path.abspath(os.path.join(__file__, '../data.json'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
