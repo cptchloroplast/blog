3fb6b39b0065bcced20b46efdd738238fb5450ce
Benjamin Okkema <ben@elm.okkema.org>
2021-09-29 10:31:21 -0500

    Add input

---
+++ src/components/Input.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let value
+  export let placeholder
+</script>
+
+<input 
+  bind:value={value} 
+  placeholder={placeholder}
+/>
+
+<style>
+  input {
+    padding: 10px;
+  }
+</style>
