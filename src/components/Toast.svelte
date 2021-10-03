3c0b06d47325b1f70b973f499848962ad654778e
Benjamin Okkema <ben@okkema.org>
2021-10-02 20:55:16 -0500

    Add Toast component and store

---
+++ src/components/Toast.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { toast } from "../stores/toast"
+</script>
+
+{#if $toast}
+<div class:flash={$toast}>
+  <i class={`i-${$toast.type}`}></i>
+  <span>{$toast.text}</span>
+</div>
+{/if}
+
+<style>
+  div {
+    visibility: hidden;
+    min-width: 250px;
+    margin-left: -125px;
+    background-color: darkslategray;
+    color: white;
+    text-align: center;
+    border-radius: 2px;
+    padding: 16px;
+    position: fixed;
+    z-index: 1;
+    left: 50%;
+    bottom: 30px;
+    font-size: 17px;
+  }
+
+  i {
+    margin-right: 5px;
+  }
+
+  .flash {
+    visibility: visible;
+    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  }
+
+  @-webkit-keyframes fadein {
+    from {bottom: 0; opacity: 0;} 
+    to {bottom: 30px; opacity: 1;}
+  }
+
+  @keyframes fadein {
+    from {bottom: 0; opacity: 0;}
+    to {bottom: 30px; opacity: 1;}
+  }
+
+  @-webkit-keyframes fadeout {
+    from {bottom: 30px; opacity: 1;} 
+    to {bottom: 0; opacity: 0;}
+  }
+
+  @keyframes fadeout {
+    from {bottom: 30px; opacity: 1;}
+    to {bottom: 0; opacity: 0;}
+  }
+</style>
