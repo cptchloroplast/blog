4bfc4c03919c521680e4f5ec7eda03bc4e52e190
Benjamin Okkema <ben@okkema.org>
2021-10-31 19:17:50 -0500

    Add initial terraform and worker

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "okkema"
+    workspaces {
+      name = "blog"
+    }
+  }
+}
+
+provider "cloudflare" {
+  account_id = var.cloudflare_account_id
+}
+
+resource "cloudflare_record" "domain" {
+  zone_id         = var.cloudflare_zone_id
+  name            = var.cloudflare_domain_name
+  type            = "AAAA"
+  value           = "100::"
+  proxied         = true
+  allow_overwrite = true
+}
+
+resource "cloudflare_workers_kv_namespace" "subscribers" {
+  title = "blog-subscribers"
+}
+
+locals {
+  worker_path = abspath("${path.module}/../functions/index.js")
+}
+
+resource "cloudflare_worker_script" "worker" {
+  name    = "blog"
+  content = file(local.worker_path)
+
+  kv_namespace_binding {
+    name         = "SUBSCRIBERS"
+    namespace_id = cloudflare_workers_kv_namespace.subscribers.id
+  }
+}
+
+resource "cloudflare_worker_route" "route" {
+  zone_id     = var.cloudflare_zone_id
+  pattern     = var.cloudflare_worker_route_pattern
+  script_name = cloudflare_worker_script.worker.name
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,15 @@
+variable "cloudflare_zone_id" {
+  type = string
+}
+
+variable "cloudflare_domain_name" {
+  type = string
+}
+
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "cloudflare_worker_route_pattern" {
+  type = string
+}
