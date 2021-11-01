9e7ae923e6c198bd05b2f657f29b175c8de3b01f
Benjamin Okkema <ben@okkema.org>
2021-11-01 09:48:02 -0500

    Revert worker

--- functions/index.ts
+++ functions/index.ts
@@ -3,13 +3,13 @@ addEventListener("fetch", (event: FetchEvent) => {
 })
 
 async function handleRequest(request: Request) {
-  // if (request.url.match(/^.+\/api\/.+/g)) {
+  if (request.url.match(/^.+\/api\/.+/g)) {
     return new Response(JSON.stringify(request), {
       headers: {
         "Content-Type": "application/json",
       },
       status: 200,
     })
-  // }
-  // return fetch(request)
+  }
+  return fetch(request)
 }
