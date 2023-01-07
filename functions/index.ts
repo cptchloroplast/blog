648fb3ff1233a048a6de806b9a572ec4f6bf17f2
Benjamin Okkema <okkema@araucaria.okkema.org>
2023-01-06 23:35:47 -0600

    Move webfinger

--- functions/index.ts
+++ functions/index.ts
@@ -1,9 +1,30 @@
 import { router } from "./router"
+import { json } from "./lib/utils"
+
+const webfinger = {
+  "subject": "acct:ben@okkema.org",
+  "aliases": [],
+  "links": [
+      {
+          "rel": "http://webfinger.net/rel/profile-page",
+          "type": "text/html",
+          "href": "https://ben.okkema.org"
+      },
+      {
+          "rel": "self",
+          "type": "application/activity+json",
+          "href": "https://ben.okkema.org/feed"
+      }
+  ]
+}
+
 
 export default {
   async fetch(request: Request, env: Environment, ctx: ExecutionContext) {
     if (request.url.match(/^.+\/api\/.+/g)) {
       return router.handle(request, env, ctx)
+    } else if (request.url.match(/^.+\/.well-known\/.+/g)) {
+      return json(webfinger)
     }
     return fetch(request)
   }
