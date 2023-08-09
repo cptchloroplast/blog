acab8dfd3ee2b71dc8a507ffe687db95a3d34f44
Benjamin Okkema <ben@okkema.org>
2023-08-08 22:45:50 -0500

    fix: environment

--- functions/_/@types/index.d.ts
+++ functions/_/@types/index.d.ts
@@ -2,8 +2,6 @@ type Environment = {
   MAILJET_API_KEY: string
   MAILJET_SECRET_KEY: string
   ADMIN_EMAIL: string
-  SUBSCRIBERS: KVNamespace
-  KEYS: KVNamespace
   BUCKET: R2Bucket
   RSA_PUBLIC_KEY: string
 }
