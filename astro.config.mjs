7a98816a35769ff9e89f2e02aae207b6ade42142
Benjamin Okkema <ben@okkema.org>
2024-06-30 19:40:00 -0500

    feat: cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import EnvironmentPlugin from "vite-plugin-environment";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare"
 
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    imageService: "passthrough",
+    platformProxy: {
+      enabled: true
+    },
+
+  }),
   site: "https://ben.okkema.org",
   server: {
     port: 5000
