002f0ab47eeee9abfdab2a913cb5d2149301a572
Benjamin Okkema <ben@okkema.org>
2024-07-07 22:13:15 -0500

    fix: sorting posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss from '@astrojs/rss';
 import metadata from "../metadata"
 import { getCollection } from 'astro:content';
+import { sortPosts } from '../utils/sortPosts';
 
 const { description } = metadata
 const posts = (await getCollection("posts"))
   .map(x => ({ ...x.data, slug: x.slug }))
-  .sort((a,b) => (a.published < b.published) ? 1 : -1)
+  .sort(sortPosts)
 
 export const GET = (context) => rss({
   title: context.site.origin,
