99b37255b5e4a8bbe46f8920de4035673552589d
Benjamin Okkema <ben@okkema.org>
2024-07-11 20:36:25 -0500

    refactor: formatting

--- src/services/Emailer.ts
+++ src/services/Emailer.ts
@@ -1,37 +1,36 @@
 export type Email = {
-    to: string
-    from: string
-    subject: string
-    html: string
-  }
+  to: string
+  from: string
+  subject: string
+  html: string
+}
   
-  type Emailer = {
-    send: (email: Email) => Promise<boolean>
-  }
+type Emailer = {
+  send: (email: Email) => Promise<boolean>
+}
   
-  export function Emailer(init: {
-    account: string
-    secret: string
-  }): Emailer {
-    const { account, secret } = init
-    return {
-      send: async (email) => {
-        const { to, from, subject, html } = email
-        const response = await fetch("https://api.mailjet.com/v3/send", {
-          method: "POST",
-          headers: {
-            Authorization: `Basic ${btoa(`${account}:${secret}`)}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            FromEmail: from,
-            Recipients:[{ Email: to }],
-            Subject: subject,
-            "Html-part": html,
-          }),
-        })
-        return response.ok
-      },
-    }
+export function Emailer(init: {
+  account: string
+  secret: string
+}): Emailer {
+  const { account, secret } = init
+  return {
+    send: async (email) => {
+      const { to, from, subject, html } = email
+      const response = await fetch("https://api.mailjet.com/v3/send", {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${btoa(`${account}:${secret}`)}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          FromEmail: from,
+          Recipients:[{ Email: to }],
+          Subject: subject,
+          "Html-part": html,
+        }),
+      })
+      return response.ok
+    },
   }
-  
+}
