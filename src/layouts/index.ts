44641f14a64d6e735b61fbeac52fabc15671a071
Benjamin Okkema <ben@okkema.org>
2024-07-15 20:54:33 -0500

    refactor: add layouts alias

---
+++ src/layouts/index.ts
@@ -0,0 +1,4 @@
+import BaseLayout from "./BaseLayout.astro"
+import PhotoLayout from "./PhotoLayout.astro"
+import PostLayout from "./PostLayout.astro"
+export { BaseLayout, PhotoLayout, PostLayout }
