094e7ec5cc921acb6918d5468210c63cedbba1d8
Benjamin Okkema <ben@okkema.org>
2024-07-17 22:29:13 -0500

    refactor: update component imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 import ContactForm from "./ContactForm.svelte"
+import Footer from "./Footer.astro"
 import Header from "./Header.svelte"
+import PostList from "./PostList.astro"
 import SubscribeForm from "./SubscribeForm.svelte"
-export { ContactForm, Header, SubscribeForm }
+import Tags from "./Tags.astro"
+import Toast from "./Toast.svelte"
+export { ContactForm, Footer, Header, PostList, SubscribeForm, Tags, Toast }
