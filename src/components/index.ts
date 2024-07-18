0cf33bcb8a0113a782a24f85a80f2367ba333f24
Benjamin Okkema <ben@okkema.org>
2024-07-17 21:35:36 -0500

    refactor: title and subtitle imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import Card from "./Card.astro"
 import ContactForm from "./ContactForm.svelte"
 import SubscribeForm from "./SubscribeForm.svelte"
-export { Card, ContactForm, SubscribeForm }
+import Subtitle from "./Subtitle.astro"
+import Title from "./Title.astro"
+export { Card, ContactForm, SubscribeForm, Subtitle, Title }
