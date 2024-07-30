8c813fa0143a27f1c08d046a9ca7d6de39decdc5
Benjamin Okkema <ben@okkema.org>
2024-07-29 22:09:32 -0500

    feat: add strava gear schema

---
+++ src/schemas/strava/Gear.ts
@@ -0,0 +1,19 @@
+import { z } from "astro:content"
+export const GearSchema = z.object({
+    brand_name: z.string().nullable(),
+    components: z.array(z.object({
+        added: z.string().datetime(),
+        brand: z.string(),
+        distance: z.number(),
+        id: z.string(),
+        model: z.string(),
+        removed: z.boolean().nullable(),
+        type: z.string(),
+    })),
+    description: z.string().nullable(),
+    frame_type: z.string(),
+    id: z.string(),
+    model_name: z.string().nullable(),
+    name: z.string()
+})
+export type Gear = z.infer<typeof GearSchema>
