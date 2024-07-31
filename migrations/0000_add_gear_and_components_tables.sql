30eb43086e1a0405be33281f6b1b1a88752e1d4e
Benjamin Okkema <ben@okkema.org>
2024-07-30 21:46:37 -0500

    feat: add orm and migrations

---
+++ migrations/0000_add_gear_and_components_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `components` (
+	`added` text NOT NULL,
+	`brand` text,
+	`distance` real,
+	`gear_id` text NOT NULL,
+	`id` text PRIMARY KEY NOT NULL,
+	`string` text,
+	`removed` text,
+	`type` text NOT NULL,
+	FOREIGN KEY (`gear_id`) REFERENCES `gear`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `gear` (
+	`brand_name` text,
+	`description` text,
+	`frame_type` text,
+	`id` text PRIMARY KEY NOT NULL,
+	`model_name` text,
+	`name` text NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX `type_idx` ON `components` (`type`);--> statement-breakpoint
+CREATE INDEX `gear_id_idx` ON `components` (`gear_id`);--> statement-breakpoint
+CREATE INDEX `name_idx` ON `gear` (`name`);
