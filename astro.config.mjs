6724969b008e89346441e0b194a4949fe5b35058
Benjamin Okkema <ben@okkema.org>
2024-08-25 21:01:43 -0500

    feat: switch markdown engine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import EnvironmentPlugin from "vite-plugin-environment"
 import svelte from "@astrojs/svelte"
 import { defineConfig } from "astro/config"
 import cloudflare from "@astrojs/cloudflare"
-import markdown from "@astropub/md"
 
 export default defineConfig({
   output: "server",
@@ -17,26 +16,9 @@ export default defineConfig({
     port: 5000
   },
   integrations: [
-    markdown(),
     svelte(),
   ],
   vite: {
     plugins: [EnvironmentPlugin(["CF_PAGES_COMMIT_SHA", "HCAPTCHA_SITEKEY"])]
   },
-  markdown: {
-    remarkPlugins: ['remark-gfm'],
-    rehypePlugins: [['rehype-external-links', {
-      target: "_blank",
-      content: {
-        type: "element",
-        tagName: "i",
-        properties: {
-          className: ["i-external"]
-        }
-      },
-      contentProperties: {
-        style: "margin-left: 4px;"
-      }
-    }]]
-  }
 })
--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,5 +1,6 @@
 import { PostSchema, type Post } from "@schemas"
 import yaml from "js-yaml"
+import { marked } from "marked"
 
 export function sortPosts(a: Post, b: Post) {
   const dateA = a.updated ?? a.published
@@ -20,4 +21,20 @@ export function parseMarkdown(markdown: string, slug: string): Post {
   const post = { ...data, content, slug }
   PostSchema.parse(post)
   return post
+}
+
+export async function renderMarkdown(markdown: string) {
+  let html = await marked.parse(markdown)
+  const regex = /<a\s+href=(?:"([^"]+)"|'([^']+)').*?>(.*?)<\/a>/g
+  const matches = Array.from(html.matchAll(regex))
+  if (matches.length) {
+    for (const [link, href, _, content] of matches) {
+      if (href != "/") {
+        const external = link.replace(content, `${content}<span style="margin-left: 4px;"><i class="i-external"></i></span>`)
+          .replace(href, `${href}" target="_blank`)
+        html = html.replace(link, external)
+      }
+    }
+  }
+  return html
 }
