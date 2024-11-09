0f743841cb4881f12dfcc05c62fb113a38943284
Benjamin Okkema <ben@okkema.org>
2024-11-08 21:00:26 -0600

    fix: middleware router dependency issue

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,18 +1,21 @@
 import type { APIContext, MiddlewareNext, MiddlewareHandler } from "astro"
 import { sequence } from "astro:middleware"
-import micromatch from "micromatch"
 import { MetadataMiddleware } from "./MetadataMiddleware"
 
 function MiddlewareRouter(router: Record<string, MiddlewareHandler>) {
-  const entries = Object.entries(router)
-  return function(context: APIContext, next: MiddlewareNext) {
-    return sequence(
-      ...entries.filter(([path]) => micromatch.isMatch(context.url.pathname, path))
-        .map(([_, handler]) => handler)
-    )(context, next)
-  }
+    const entries = Object.entries(router)
+    return function (context: APIContext, next: MiddlewareNext) {
+        return sequence(
+            ...entries.filter(function ([path]) {
+                return context.url.pathname.match(RegExp(`^${(path
+                    .replace(/\/+(\/|$)/g, '$1')) // strip double & trailing splash
+                    .replace(/(\/?)\*/g, '($1.*)?') // wildcard
+                    }/*$`))
+            }).map(([_, handler]) => handler)
+        )(context, next)
+    }
 }
 
 export const onRequest = MiddlewareRouter({
-    "*": MetadataMiddleware
+    "*": MetadataMiddleware,
 })
