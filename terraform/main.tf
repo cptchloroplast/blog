24dba3c49c21d573b7eb41eec8c5ac18f7a78f64
Benjamin Okkema <ben@okkema.org>
2024-11-13 20:07:11 -0600

    fix: cloudflare application subdomain

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,6 +122,5 @@ module "application" {
   zone_id      = var.cloudflare_zone_id
   name         = var.pages_hostname
   github_teams = [var.github_repository]
-  use_root     = true
   path         = "admin"
 }
