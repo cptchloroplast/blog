a5d585861cebeed377a7f89e94c512cabcb30986
Benjamin Okkema <ben@okkema.org>
2024-11-13 21:25:31 -0600

    feat: extract public layout

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,5 @@
 import BaseLayout from "./BaseLayout.astro"
 import PhotoLayout from "./PhotoLayout.astro"
 import PostLayout from "./PostLayout.astro"
-export { BaseLayout, PhotoLayout, PostLayout }
+import PublicLayout from "./PublicLayout.astro"
+export { BaseLayout, PhotoLayout, PostLayout, PublicLayout }
