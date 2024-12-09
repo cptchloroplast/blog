cc171cbb968326a6f35df9d06040af89a2d7aaa9
Benjamin Okkema <ben@okkema.org>
2024-12-08 22:17:13 -0600

    feat: update rss feed

--- src/utils/feeds.ts
+++ src/utils/feeds.ts
@@ -1,12 +1,28 @@
 import type { Metadata, Post } from "@schemas"
 import { formatDate } from "./posts"
 
+const symbol = "\u00A9"
+
+export function copyright(date: Date) {
+  const year = date.getUTCFullYear()
+  const current = new Date().getUTCFullYear()
+  if (year < current) return `${symbol} ${year}-${current}`
+  else if (year > current) return `${symbol} ${current}-${year}`
+  else return `${symbol} ${year}`
+}
+
 export function rss(origin: string, metadata: Metadata, posts: Post[]) {
+  const earliest = posts[posts.length - 1]
+  const latest = posts[0]
   return '<rss version="2.0"><channel>'
     + `<title>${metadata.title}</title>`
+    + "<image><url>https://okkema.org/logo.png</url></image>"
     + `<description>${metadata.description}</description>`
     + `<link>${origin}</link>`
     + "<language>en-us</language>"
+    + `<copyright>${copyright(earliest.published!)} ${metadata.name}</copyright>`
+    + `<webMaster>${metadata.email}</webMaster>`
+    + `<pubDate>${formatDate(latest.published!)}</pubDate>`
     + posts.map(post => item(origin, post)).join("")
     + "</channel></rss>"
 }
@@ -15,8 +31,10 @@ function item(origin: string, post: Post) {
   return "<item>"
     + `<title>${post.title}</title>`
     + `<link>${origin}/posts/${post.slug}</link>`
+    + `<description>${post.content}</description>`
     + `<guid isPermaLink="true">${origin}/posts/${post.slug}</guid>`
     + `<pubDate>${formatDate(post.updated ?? post.published!)}</pubDate>`
+    + post.tags.map(tag => `<category>${tag}</category>`).join("")
     + "</item>"
 }
 
